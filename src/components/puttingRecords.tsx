983a1f8de2077e30c20c89df3fea84b981d7cf42
Espen Halvorsen <espen.halvorsen@statkraft.com>
2018-09-11 09:07:33 +0200

    [records] Start cleaning up the records widget.

--- src/components/puttingRecords.tsx
+++ src/components/puttingRecords.tsx
@@ -15,6 +15,15 @@ interface IPuttingRecordsPropFields {
     bestMonth?: { month: moment.Moment, score: IScoreAggregation };
     bestQuarter?: { quarter: moment.Moment, score: IScoreAggregation };
     bestYear?: { year: moment.Moment, score: IScoreAggregation };
+    records: IPuttingRecordsRow[];
+}
+
+export interface IPuttingRecordsRow {
+    what: string;
+    value: number;
+    who: IPutter;
+    when?: string;
+    how?: IRoundScore[];
 }
 
 class PuttingRecordsView extends React.Component<IPuttingRecordsPropFields, {}> {
@@ -24,52 +33,17 @@ class PuttingRecordsView extends React.Component<IPuttingRecordsPropFields, {}> 
             containerClass="records"
             title={{ text: "Records", icon: faTrophy }}
         >
-            <table className="records-table">
-                <tbody>
-                    {this.props.mostPuts && <tr>
-                        <th scope="row">Most puts</th>
-                        <td className="value">{this.props.mostPuts.count}</td>
-                        <td>{this.props.mostPuts.putter.name}</td>
-                        <td>{}</td>
-                        <td>{}</td>
-                    </tr>}
-                    {this.props.longestStreak && <tr>
-                        <th scope="row">Longest streak</th>
-                        <td className="value">{this.props.longestStreak.streak.length}</td>
-                        <td>{this.props.longestStreak.putter.name}</td>
-                        <td className="when">{this.props.longestStreak.streak.start.format("DD MMM YY")}</td>
-                        <td><ScoreBulletList scores={this.props.longestStreak.streak.scores} /></td>
-                    </tr>}
-                    {this.props.bestWeek && <tr>
-                        <th scope="row">Best week</th>
-                        <td className="value">{this.props.bestWeek.score.scoreSum}</td>
-                        <td>{this.props.bestWeek.score.putter.name}</td>
-                        <td className="when">From: {this.props.bestWeek.week.format("DD MMM YY")}</td>
-                        <td><ScoreBulletList scores={this.props.bestWeek.score.scores} /></td>
-                    </tr>}
-                    {this.props.bestMonth && <tr>
-                        <th scope="row">Best month</th>
-                        <td className="value">{this.props.bestMonth.score.scoreSum}</td>
-                        <td>{this.props.bestMonth.score.putter.name}</td>
-                        <td className="when">{this.props.bestMonth.month.format("MMMM YY")}</td>
-                        <td><ScoreBulletList scores={this.props.bestMonth.score.scores} /></td>
-                    </tr>}
-                    {this.props.bestQuarter && <tr>
-                        <th scope="row">Best quarter</th>
-                        <td className="value">{this.props.bestQuarter.score.scoreSum}</td>
-                        <td>{this.props.bestQuarter.score.putter.name}</td>
-                        <td className="when">Q{this.props.bestQuarter.quarter.quarter() + " " + this.props.bestQuarter.quarter.format("YY")}</td>
-                        <td>{}</td>
-                    </tr>}
-                    {this.props.bestYear && <tr>
-                        <th scope="row">Best year</th>
-                        <td className="value">{this.props.bestYear.score.scoreSum}</td>
-                        <td>{this.props.bestYear.score.putter.name}</td>
-                        <td className="when">{this.props.bestYear.year.format("YYYY")}</td>
-                        <td>{}</td>
-                    </tr>}
-                </tbody>
-            </table>
+            <div className="records-list">
+                {this.props.records.map((record, index) => {
+                    return <div className="records-item" key={index}>
+                        <div className="what">{record.what}</div>
+                        <div className="value">{record.value}</div>
+                        <div className="who">{record.who}</div>
+                        <div className="when">{record.when}</div>
+                        <div className="how">{record.how && <ScoreBulletList scores={record.how} />}</div>
+                    </div>;
+                })}
+            </div>
         </Widget>;
     }
 }
@@ -86,32 +60,47 @@ const mapStateToProps = (state: IApplicationState): IPuttingRecordsPropFields =>
         };
     });
 
-    // Best week and month.
-    const bestWeek = ScoreSelectors.getBestPartition(state, "isoWeek");
-    const bestMonth = ScoreSelectors.getBestPartition(state, "month");
-    const bestQuarter = ScoreSelectors.getBestPartition(state, "quarter");
-    const bestYear = ScoreSelectors.getBestPartition(state, "year");
+    const records: IPuttingRecordsRow[] = [];
+
+    const addAggregation = (what: string, thing: { partitionTick: number, score: IScoreAggregation } | undefined, dateFormatter: (date: moment.Moment) => string) => {
+        if (thing) {
+            records.push({
+                what: what,
+                value: thing.score.scoreSum,
+                who: thing.score.putter,
+                how: thing.score.scores,
+                when: dateFormatter(moment(thing.partitionTick))
+            });
+        }
+    };
+
+    const mostPuts = _.first(_.orderBy(countForPlayer, [s => s.count, s => s.latestScore], "desc"));
+    if (mostPuts) {
+        records.push({
+            what: "Most puts",
+            value: mostPuts.count,
+            who: mostPuts.putter
+        });
+    }
+
+    const longestStreak = _.maxBy(ScoreSelectors.getStreaks(state), p => p.streak.length);
+    if (longestStreak) {
+        records.push({
+            what: "Longest streak",
+            value: longestStreak.streak.length,
+            who: longestStreak.putter,
+            how: longestStreak.streak.scores,
+            when: longestStreak.streak.start.format("DD MMM YY")
+        });
+    }
+
+    addAggregation("Best week", ScoreSelectors.getBestPartition(state, "isoWeek"), date => `From: ${date.format("DD MMM YY")}`);
+    addAggregation("Best month", ScoreSelectors.getBestPartition(state, "month"), date => date.format("MMMM YY"));
+    addAggregation("Best quarter", ScoreSelectors.getBestPartition(state, "quarter"), date => `Q${date.quarter()} ${date.format("YY")}`);
+    addAggregation("Best year", ScoreSelectors.getBestPartition(state, "year"), date => date.format("YYYY"));
 
     return {
-        // Prefer the last putter, if equal.
-        mostPuts: _.first(_.orderBy(countForPlayer, [s => s.count, s => s.latestScore], "desc")),
-        longestStreak: _.maxBy(ScoreSelectors.getStreaks(state), p => p.streak.length),
-        bestWeek: bestWeek && {
-            week: moment(bestWeek.partitionTick),
-            score: bestWeek.score
-        },
-        bestMonth: bestMonth && {
-            month: moment(bestMonth.partitionTick),
-            score: bestMonth.score
-        },
-        bestQuarter: bestQuarter && {
-            quarter: moment(bestQuarter.partitionTick),
-            score: bestQuarter.score
-        },
-        bestYear: bestYear && {
-            year: moment(bestYear.partitionTick),
-            score: bestYear.score
-        }
+        records
     };
 };
 
